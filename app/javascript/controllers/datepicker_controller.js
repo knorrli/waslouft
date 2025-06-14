2c46adb8289289c3ec98a5a721e4ed1795d72736
Simon Kiener <jugglinghobo@gmail.com>
2025-06-14 04:21:11 +0200

    fix date handling for datepicker ranges

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -50,16 +50,9 @@ export default class extends Controller {
           this.element.dispatchEvent(new Event('change', { bubbles: true }));
         });
         picker.on('view', (e) => {
-          console.log(e.detail.view);
           if (e.detail.view == 'PresetPluginButton') {
             const startDate = picker.getStartDate();
             const endDate = picker.getEndDate();
-            console.log("start date", startDate?.getTime());
-            console.log("end date", endDate?.getTime());
-            // if (e.detail.target.classList.contains('start') && e.detail.target.classList.contains('end')) {
-            //   if (endDate?.getTime()?.toString() === e.detail.target.dataset.end) {
-            //   e.detail.target.classList.add('active');
-            //   }
             if (startDate?.getTime()?.toString() === e.detail.target.dataset.start && endDate?.getTime()?.toString() === e.detail.target.dataset.end) {
               e.detail.target.classList.add('active');
             }
@@ -145,16 +138,20 @@ export default class extends Controller {
     const presetPlugin = this.datepicker.PluginManager.addInstance("PresetPlugin");
 
     const now = new Date();
-    let today = new Date(now.getFullYear(), now.getMonth(), now.getDay()); today.setDate(now.getDate());
-    let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDay()); tomorrow.setDate(now.getDate() + 1);
-    let currentWeekendStart = new Date(now.getFullYear(), now.getMonth(), now.getDay()); currentWeekendStart.setDate(now.getDate() + (6+((1*7)-now.getDay())));
-    let currentWeekendEnd = new Date(now.getFullYear(), now.getMonth(), now.getDay()); currentWeekendEnd.setDate(now.getDate() + (0+((2*7)-now.getDay())));
-    let startOfNextWeek = new Date(now.getFullYear(), now.getMonth(), now.getDay()); startOfNextWeek.setDate(now.getDate() + (1+((1*7)-now.getDay())));
-    let endOfNextWeek = new Date(now.getFullYear(), now.getMonth(), now.getDay()); endOfNextWeek.setDate(now.getDate() + (0+((2*7)-now.getDay())));
-    let nextWeekendStart = new Date(now.getFullYear(), now.getMonth(), now.getDay()); nextWeekendStart.setDate(now.getDate() + (6+((2*7)-now.getDay())));
-    let nextWeekendEnd = new Date(now.getFullYear(), now.getMonth(), now.getDay()); nextWeekendEnd.setDate(now.getDate() + (0+((3*7)-now.getDay())));
+    let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate()); tomorrow.setDate(now.getDate() + 1);
+
+    let startOfCurrentWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate()); startOfCurrentWeek.setDate(this.#getWeekday(now, 0, 0));
+    let startOfCurrentWeekend = new Date(now.getFullYear(), now.getMonth(), now.getDate()); startOfCurrentWeekend.setDate(this.#getWeekday(now, 5, 0));
+    let endOfCurrentWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate()); endOfCurrentWeek.setDate(this.#getWeekday(now, 6, 0))
+
+    let startOfNextWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate()); startOfNextWeek.setDate(this.#getWeekday(now, 0, 1))
+    let startOfNextWeekend = new Date(now.getFullYear(), now.getMonth(), now.getDate()); startOfNextWeekend.setDate(this.#getWeekday(now, 5, 1));
+    let endOfNextWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate()); endOfNextWeek.setDate(this.#getWeekday(now, 6, 1));
+
     let firstDayOfCurrentMonth = new Date(now.getFullYear(), now.getMonth(), 1);
     let lastDayOfCurrentMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+
     let firstDayOfNextMonth = new Date(now.getFullYear(), now.getMonth() + 1, 1);
     let lastDayOfNextMonth = new Date(now.getFullYear(), now.getMonth() + 2, 0);
 
@@ -162,9 +159,10 @@ export default class extends Controller {
       customPreset: {
         'Heute': [today, today],
         'Morgen': [tomorrow, tomorrow],
-        'dieses Wochenende': [currentWeekendStart, currentWeekendEnd],
+        'diese Woche': [startOfCurrentWeek, endOfCurrentWeek],
+        'dieses Wochenende': [startOfCurrentWeekend, endOfCurrentWeek],
         'nächste Woche': [startOfNextWeek, endOfNextWeek],
-        'nächstes Wochenende': [nextWeekendStart, nextWeekendEnd],
+        'nächstes Wochenende': [startOfNextWeekend, endOfNextWeek],
         'aktueller Monat': [firstDayOfCurrentMonth, lastDayOfCurrentMonth],
         'nächster Monat': [firstDayOfNextMonth, lastDayOfNextMonth]
       },
@@ -181,4 +179,37 @@ export default class extends Controller {
   get presetPluginEnabled() {
     return this.enablePresetValue;
   }
+
+  #getNextDay(dayName) {
+
+    // The current day
+    var date = new Date();
+    var now = date.getDay();
+
+    // Days of the week
+    var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+    // The index for the day you want
+    var day = days.indexOf(dayName.toLowerCase());
+
+    // Find the difference between the current day and the one you want
+    // If it's the same day as today (or a negative number), jump to the next week
+    var diff = day - now;
+    diff = diff < 1 ? 7 + diff : diff;
+
+    // Get the timestamp for the desired day
+    var nextDayTimestamp = date.getTime() + (1000 * 60 * 60 * 24 * diff);
+
+    // Get the next day
+    return new Date(nextDayTimestamp);
+
+  };
+
+  #getWeekday(date, weekDay, weekInFuture) {
+    return new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay() + ((date.getDay() == 0 ? -6 : 1) + (weekInFuture * 7)) + weekDay).getDate();
+  }
+
+  // function getWeekday(date, weekDay, weekInFuture) {
+  //   return new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay() + ((date.getDay() == 0 ? -6 : 1) + (weekInFuture * 7)) + weekDay).getDate();
+  // }
 }

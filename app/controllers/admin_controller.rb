1469c914d6ff8eeaa25e14f86ee03ce37b96ef26
Simon Kiener <jugglinghobo@gmail.com>
2025-06-20 17:31:35 +0200

    use upcased genres for styles

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,7 +6,7 @@ class AdminController < ApplicationController
     json = JSON.parse(File.read('./lib/genres.json'))
     json.each do |style, genres|
       style = Style.find_or_initialize_by(name: style)
-      style.update(genre_list: genres)
+      style.update(genre_list: genres.map(&:titleize))
     end
     redirect_to admin_path
   end
